f7cc9ee611a0274998557426e2ab26fc4d8d10e3
edward <edhuosb18mail.com>
2020-12-28 10:00:54 -0500

    modificasiones 4

--- docs/public/js/politica__envio.js
+++ docs/public/js/politica__envio.js
@@ -11,3 +11,22 @@ addEventListener("scroll",()=>{
     
     
     })
+
+
+
+
+
+
+        addEventListener("scroll",()=>{
+            let certificaciones=document.getElementById("politica__animacion")
+            let altura=window.innerHeight/1.1
+            let distancia=certificaciones.getBoundingClientRect().top
+            const rootStyles=document.documentElement.style
+            if(distancia <= altura){
+               
+                rootStyles.setProperty("--stop7","running")
+            
+            }
+            
+            
+            })
